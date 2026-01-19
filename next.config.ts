5f48b0dc5760c3d9a7c4563e75c848715725e371
Sumeet Sharma <blank.gamer132@gmail.com>
2026-01-19 20:29:48 +0530

    vercel fix

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "john-doe-fb",
+  org: "dev-flow",
 
-  project: "polaris",
+  project: "javascript-nextjs",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
