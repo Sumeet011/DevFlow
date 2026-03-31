48c2ff0eafe9212012905b2aa73cdecfc3436bf0
Sumeet Sharma <blank.gamer132@gmail.com>
2026-03-31 10:29:18 +0530

    Done

--- next.config.ts
+++ next.config.ts
@@ -26,9 +26,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "john-doe-fb",
-
-  project: "polaris",
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
